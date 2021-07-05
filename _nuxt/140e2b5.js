4040cce7a6374f0d5f014a6dc69d1036d2c429e5
xayrullo <xayrullo.jaloldinov@gmail.com>
2021-07-05 18:21:22 +0500

    adding vue-gallaries

---
+++ _nuxt/140e2b5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{416:function(t,e,n){"use strict";n.r(e);var l={},c=n(63),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"bg-white shadow-md animate-pulse"})}),[],!1,null,null,null);e.default=component.exports}}]);
